25da4fc2e47552e1ee0af25426b0dfc4d01dc833
Moisés Rocha <moisesrocha99@gmail.com>
2021-01-23 01:37:36 +0000

    fibonacci heap implementation

--- test/test_fibonacci_heap.cpp
+++ test/test_fibonacci_heap.cpp
@@ -1,10 +1,10 @@
-#include "binary_heap.hpp"
+#include "fibonacci_heap.hpp"
 #include "catch2/catch.hpp"
 #include <iostream>
 
-SCENARIO("binary heap changes size", "[binary_heap]") {
+SCENARIO("heap has correct size", "[fibonacci_heap]") {
     GIVEN("binary heap has some items") {
-        BinaryHeap<int> heap;
+        fibonacci_heap<int> heap;
 
         heap.push(4);
         heap.push(2);
@@ -30,47 +30,50 @@ SCENARIO("binary heap changes size", "[binary_heap]") {
     }
 }
 
-SCENARIO("binary heap sorts items", "[binary_heap]") {
+SCENARIO("heap sorts items", "[fibonacci_heap]") {
     GIVEN("binary heap has some items") {
-        BinaryHeap<int> heap;
+        fibonacci_heap<int> heap;
 
         heap.push(4);
         heap.push(2);
-        heap.push(10);
         heap.push(-3);
         heap.push(1);
+        heap.push(10);
 
-        REQUIRE( heap.top() == -3 );
-        
+        REQUIRE( heap.size() == 5 );
+
         WHEN("new min is added") {
             heap.push(-4);
             THEN("the heaps top changes") {
-                REQUIRE( heap.top() == -4 );
+                REQUIRE( heap.peek() == -4 );
             }
         }
 
         WHEN("non min value is added") {
             heap.push(11);
             THEN("the heaps top doesn't change") {
-                REQUIRE( heap.top() == -3 );
+                REQUIRE( heap.peek() == -3 );
             }
         }
 
         WHEN("value is poped from the heap") {
             heap.pop();
-            THEN("the heaps top changes") {
-                REQUIRE( heap.top() == 1 );
+            THEN("the heap's top changes") {
+                REQUIRE( heap.peek() == 1 );
             }
         }
 
         WHEN("all values are popped from the heap") {
             std::vector<int> popped;
             while (!heap.empty()) {
-                popped.push_back(heap.top());
+                int temp = heap.peek();
+                std::cout << temp << std::endl;
+                popped.push_back(temp);
                 heap.pop();
             }
             THEN("they come out in ascending order") {
-                REQUIRE( std::is_sorted(std::begin(popped), std::end(popped)) );
+                std::vector<int> test = {-3, 1, 2, 4, 10};
+                REQUIRE(std::equal(test.begin(), test.end(), popped.begin()));
             }
         }
     }
